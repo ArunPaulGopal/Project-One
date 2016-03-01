d31f524d8f84723fb026288f7b2f3893ad077593
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-03-01 13:42:25 -0800

    Issue #3: Basic comment adding functionality is working.

--- default.js
+++ default.js
@@ -1,34 +1,52 @@
+//Comments Box
+
+
+//Do something here to grab comments box input/floating text and label it as a variable.
+
+var submit = document.getElementById('submit');
+var commentbox = document.getElementById('comment');
+
+
+submit.addEventListener('click',function(e) {
+  var commenttext = commentbox.value;
+  var text = document.createTextNode(commenttext);
+  var para = document.createElement('p');
+  var history = document.getElementById('history');
+  para.appendChild(text);
+  history.appendChild(para);
+})
+
+//Do something here to take that input and append it as a child of existing comments grid? Event Listener/Function
+
+
+
+
+
 // Search Results/Descriptions
 
 var videoresults = [
 {
   title: "Result 1",
-  uploadname: "UploadGuy 1",
-  uploaddate: "1/12/16",
+  uploadinfo: "UploadGuy 1 1/12/16",
   views:  571,
   description: "This is my first description of a video",
   likes:  871,
-  dislikes: 322,
   img: 'logo.jpg'
 },
 {
   title: "Result 2",
-  uploadname: "UploadGuy 2",
-  uploaddate: "1/12/16",
+  uploadinfo: "UploadGuy 1 1/12/16",
   views:  571,
   description: "This is my first description of a video",
   likes:  871,
-  dislikes: 322,
   img: 'logo.jpg'
 },
 {
   title: "Result 3",
-  uploadname: "UploadGuy 3",
-  uploaddate: "1/12/16",
+  uploadinfo: "UploadGuy 1 1/12/16",
   views:  571,
   description: "This is my first description of a video",
   likes:  871,
-  dislikes: 322,
   img: 'logo.jpg'
 },
 
@@ -55,35 +73,45 @@ function myFunction(object){
 
 
 
-  //descriptions
+  //body
+  var uploadinfo = document.createElement('p');
+  var ucontent = object.uploadinfo;
+  var utext = document.createTextNode(ucontent);
 
 
+
+
+  var title = document.createElement('h1');
+  title.className=("media-heading");
+  var tcontent = object.title;
+  var ttext = document.createTextNode(tcontent);
   var mediabody = document.createElement('div');
   mediabody.className= ("media-body");
   var paragraph = document.createElement('p');
   var content = object.description;
   var text = document.createTextNode(content);
 
-
+  uploadinfo.appendChild(utext);
   paragraph.appendChild(text);
+  title.appendChild(ttext);
+  mediabody.appendChild(title);
   mediabody.appendChild(paragraph);
+  mediabody.appendChild(uploadinfo);
   media.appendChild(mediabody);
 
 
 
   results.appendChild(media);
 
-
-
-
-//headers
-
-
-
 }
 
 var results = document.getElementById('resultsview');
-console.log(results);
+
 results.addEventListener('click',function(e) {
-    myFunction(videoresults[0]);
+  for (var i=0; i<videoresults.length; i++){
+    myFunction(videoresults[i]);
+  }
+
+//TO FINISH: ADD SEARCH KEYWORD ID AND USE THAT TO SERVE AS THE IF STATEMENT FOR RUNNING THINGS
+
 });
