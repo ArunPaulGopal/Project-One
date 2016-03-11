d6812062a3930efbae2bd561a9f232d566fb8ff0
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-03-11 13:09:50 -0800

    Bootstrap: Minor panel heading changes.

--- default.js
+++ default.js
@@ -302,7 +302,8 @@ $(document).ready(function(){
         url: "https://www.youtube.com/embed/VaMjhwFE1Zw",
         buttonid: "toggleiceman",
         rowid: "icerow",
-        starid: false
+        starid: false,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         title: "Neil Gaiman: 2012 Commencement Speech",
@@ -315,7 +316,8 @@ $(document).ready(function(){
         url: "https://www.youtube.com/embed/plWexCID-kA",
         buttonid: "togglespeech",
         rowid: "speechrow",
-        starid: false
+        starid: false,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         title: "Steve Jobs: 2005 Stanford Commencement",
@@ -328,7 +330,8 @@ $(document).ready(function(){
         url: "https://www.youtube.com/embed/D1R-jKKp3NA",
         buttonid: "togglejobs",
         rowid: "jobsrow",
-        starid: false
+        starid: false,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       },
       {
         title: "Senecca: Health, Happiness, and Stoicism",
@@ -341,7 +344,8 @@ $(document).ready(function(){
         url: "https://www.youtube.com/embed/EYWEAa-D5vM",
         buttonid: "togglesenecca",
         rowid: "seneccarow",
-        starid: false
+        starid: false,
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
       }
     ];
   // SEARCH RESULTS: Create hidden videos upon search
@@ -400,15 +404,20 @@ $(document).ready(function(){
     panelbody.setAttribute('class','panel-body');
     var panelfooter = document.createElement('div');
     panelfooter.setAttribute('class','panel-footer text-center');
+    var textparagraph = document.createElement('p');
+    var textfiller = object.text;
+    var textnode = document.createTextNode(textfiller);
     // Appending body variables
     button.appendChild(buttontext);
     attribute.appendChild(button);
     uploadinfo.appendChild(uploadtext);
     paragraph.appendChild(text);
+    textparagraph.appendChild(textnode);
     title.appendChild(titletext);
     mediabody.appendChild(title);
     mediabody.appendChild(uploadinfo);
     mediabody.appendChild(paragraph);
+    mediabody.appendChild(textparagraph);
     media.appendChild(mediabody);
     panelbody.appendChild(media);
     panel.appendChild(panelbody);
